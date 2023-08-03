2666727e3aebf24e905d6f70caa9f94b985d7f5c
Cura <cura9801@gmail.com>
2023-08-03 14:54:48 +0800

    上傳lv1作業

---
+++ assets/scss/JS/all.js
@@ -0,0 +1,32 @@
+$(document).ready(function () {
+    $(".nav__menu__list--1st").hover(function () {
+            // over
+        $(".bd1").addClass("bd1--hover");
+        }, function () {
+        $(".bd1").removeClass("bd1--hover");
+            // out
+        }
+    );
+    $(".nav__menu__list--2nd").hover(function () {
+            // over
+        $(".bd2").addClass("bd2--hover");
+        }, function () {
+        $(".bd2").removeClass("bd2--hover");
+            // out
+        }
+    );
+    $(".nav__menu__list--3rd").hover(function () {
+            // over
+        $(".bd3").addClass("bd3--hover");
+        }, function () {
+        $(".bd3").removeClass("bd3--hover");
+            // out
+        }
+    );
+    
+   
+   
+
+
+
+});
